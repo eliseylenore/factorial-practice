37e0ca507fd5227cf74f66e3488ac45cc198ebbf
Elise St Hilaire and Kory Skarbek <elise@>
2017-01-17 15:11:17 -0800

    Add div to show/hide after user inputs answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    $("#factorial-result").hide();
+    $("#factorial-result").show();
+
     var userInput = parseInt($("#user-number").val());
     var result = 1;
     for (i = userInput; i > 0; i -= 1) {
       result *= i;
-      console.log(result);
     }
+    $("#input").text(userInput);
+    $("#result").text(result);
   });
 });
